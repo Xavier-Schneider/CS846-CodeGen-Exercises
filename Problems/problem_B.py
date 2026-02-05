f680a544922687be3384df3909a3408348a44fa0
Greg French <gregfrench26@gmail.com>
2026-02-04 20:09:45 -0500

    Added Problem B skeleton code

---
+++ Problems/problem_B.py
@@ -0,0 +1,14 @@
+class SudokuPuzzleValidator:
+    grid = None
+
+    def __init__(self, grid=None):
+        self.grid = grid
+
+    def validate_rows(self):
+        pass
+    
+    def validate_cols(self):
+        pass
+    
+    def validate_boxes(self):
+        pass
